9c1c13644ba93f71c30c1f80f24c272b39ad1887
datelohan <datelohan@gmail.com>
2022-06-21 21:46:19 -0300

    commit inicial

---
+++ ex12.js
@@ -0,0 +1,8 @@
+var agora = new Date()
+var hora = agora.getHours()
+if(hora <12){
+console.log(`Agora são ${hora} horas Bom Dia !` )
+}else if (hora <18){
+console.log(`Agora são ${hora} horas Boa Tarde !`)
+} else
+console.log(`Agora são ${hora} horas Boa Noite !`)
